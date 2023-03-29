9608085a046203252cb911b2f04f472321ea05d4
Bilal094 <99066318@mydavinci.nl>
2023-03-29 11:40:20 +0200

    Finished whole assignment

--- Backend_2/J2F1BELP2L3/welcome.php
+++ Backend_2/J2F1BELP2L3/welcome.php
@@ -1,3 +1,47 @@
+
+<?php 
+
+    $name = $email = '';
+    $nameErr = $emailErr = '';
+    $nameBool = false;
+    $emailBool = false;
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        if (empty($_POST['name'])) {
+            $nameErr = 'Naam is verplicht';
+        } else {
+            $name = test_input($_POST['name']);
+            $nameBool = true;
+        }
+
+        if (empty($_POST['email'])) {
+            $emailErr = 'Email is verplicht';
+        } elseif (strpos($_POST["email"], '@') === false && strpos($_POST["email"], '.') === false) {
+            $emailErr = 'Email is niet geldig';
+        } else {
+            $email = test_input($_POST['email']);
+            $emailBool = true;
+        }
+    }
+
+    if ($nameBool && $emailBool === true) {
+        echo '<div>';
+        echo '<script>console.log(\'Valid Name\');</script>';
+        echo "<h1>Ingevulde gegevens zijn</h1>";
+        echo "Naam: " . $name . " <br>";
+        echo "Email: ". $email;
+        echo '</div>';
+    }
+
+    function test_input($formData) {
+        $formData = trim($formData);
+        $formData = stripslashes($formData);
+        $formData = htmlspecialchars($formData);
+        return $formData;
+    }
+    
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,43 +55,17 @@
 
     <div id="input">
     <form action=<?php echo htmlspecialchars($_SERVER["PHP_SELF"]) ?> method="post">
-        <label for="name">Naam:</label> 
-        <span><?php if (isset($nameErr)) echo $nameErr;?></span><br>
-        <input type="text" name="name"> <br> <br>
+        <label for="name">Naam:</label> <br>
+        <input type="text" name="name"> <br>
+        <span><?php if (isset($nameErr)) echo $nameErr; ?></span> <br> <br>
 
         <label for="email">Email:</label> <br>
-        <input type="email" name="email"> <br> <br>
+        <input type="email" name="email"> <br>
+        <span><?php if (isset($emailErr)) echo $emailErr; ?></span> <br> <br>
         
         <input type="submit" value="Verzend" name="submit"> 
+
     </form>
-
-    <?php 
-
-        $name = $email = '';
-
-        if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            if (empty($_POST['name'])) {
-                print 'Unvalid Name';
-                $nameErr = 'Naam is verplicht';
-            } else {
-                print 'Valid Name';
-                $name = $_POST['name'];
-                test_input($name);
-            }
-
-
-
-            echo "<h1>Ingevulde gegevens zijn</h1>";
-            echo "Naam: " . $name . " <br>";
-            echo "Email: ". $email;
-        }
-
-        function test_input($formData) {
-            $formData = trim($formData);
-            $formData = stripslashes($formData);
-            $formData = htmlspecialchars($formData);
-        }
-    ?>
     </div>
 </body>
 </html>
