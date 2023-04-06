3d93100c04c096eadd4a0f46302604d7c0ca3b19
Bilal Achrifi <bilalachrifi@hotmail.com>
2023-04-07 01:55:58 +0200

    finished assigment

---
+++ Backend_3/J2F1BELP3L1-onderdeel-3/content.php
@@ -0,0 +1,13 @@
+<?php
+
+$fullname = 'Bilal Achrifi' . '<br>';
+$img = '<img src="thumbs-up.jpg" alt="thumbs up" id="img">';
+$lorem = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit sequi debitis exercitationem itaque maiores, similique iste repudiandae enim amet, eaque fuga minus, at earum nostrum nulla totam officiis deserunt sed!' . '<br>';
+
+echo '<style> #img {width: 10%;} </style>';
+
+echo $fullname . "<br>";
+echo $img;
+echo $lorem . "<br> <br>";
+
+?>
