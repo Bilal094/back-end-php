8dbe3e087ed6e0b9f8957c508383b3990b00c68f
Bilal Achrifi <bilalachrifi@hotmail.com>
2023-04-12 01:26:58 +0200

    added new folder

---
+++ Backend_5/J2F1BELP5L2/pages/islam.php
@@ -0,0 +1,19 @@
+<?php
+?>
+<html>
+    <section>
+
+        <img src="images/islam.jpg" alt="islam">
+        <p>De islam is een van de grootste religies ter wereld en heeft ongeveer 1,8 miljard volgers. Het ontstond in de 7e eeuw na Christus in wat nu Saoedi-Arabië is, en verspreidde zich snel over de hele wereld. De basis van de islam wordt gevormd door de vijf zuilen: de getuigenis van het geloof, het gebed, de armenbelasting, het vasten tijdens de Ramadan en de bedevaart naar Mekka.
+
+Een belangrijk aspect van de islam is de Koran, het heilige boek van de moslims. Het bevat de openbaringen van God aan de profeet Mohammed en is de basis van de islamitische leer en wetgeving.
+
+De islam is verdeeld in verschillende stromingen, waaronder soennisme en sjiisme. Deze stromingen verschillen in hun interpretaties van de Koran en in de praktijk van hun religieuze rituelen.
+
+De islam heeft invloed gehad op vele aspecten van het dagelijks leven van moslims, zoals de kleding, het dieet en de sociale gewoonten. Het heeft ook belangrijke bijdragen geleverd aan de wetenschap, filosofie, literatuur en kunst.
+
+Helaas is de islam ook vaak in verband gebracht met terrorisme en extremisme. Het is echter belangrijk om te onthouden dat deze extremisten slechts een kleine minderheid vormen binnen de moslimgemeenschap en dat de overgrote meerderheid van moslims vredelievende mensen zijn die hun geloof praktiseren op een manier die in overeenstemming is met de waarden van tolerantie, respect en verdraagzaamheid.</p>
+        
+    </section>
+
+</html>
