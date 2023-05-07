347d62a3ae8262a0d86036e67fa1e8626bd63ea0
Bilal Achrifi <bilalachrifi@hotmail.com>
2023-05-08 00:04:37 +0200

    Making a begin

---
+++ Backend_5/J2F1BELP5O1/index.php
@@ -0,0 +1,8 @@
+<!-- Connectie maken met pdo database, enz. en het aantal characters/info over characters hier tonen -->
+<!-- Alle pagina's zijn valide HTML (W3Cvalidator).
+Code voor karakterkaarten/detailpagina's mag je niet meer dan 1 keer hebben geschreven (je gaat dit dus dynamisch hergebruiken).
+Je database connectie is veilig en je maakt gebruik van PDO!.
+Als je een character in de database toevoegd moet alles blijven werken.
+Onderaan alle paginas staat je naam een copyright.
+Maak gebruik van includes en/of requires.
+De applicatie moet blijven werken als er 1 of meerdere characters in de database word toegevoegd en als er 1 of meerdere verwijderd worden. -->
